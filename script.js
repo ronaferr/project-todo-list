bb2a4e70d1bc19674367b010dd6df42ed73ab72e
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-02-16 18:52:04 -0300

    entrega requisitos 7

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function criarTarefa() {
     let botao = document.getElementById('criar-tarefa');
-    
-    
         /* console.log(lista); */
     botao.addEventListener('click', function() {
         let lista = document.getElementById('lista-tarefas');
         let itemLista = document.createElement('li');
+        //função para ficar cinza
+        itemLista.addEventListener('click', function() {
+                itemLista.style.backgroundColor = 'rgb(128, 128, 128)'
+            
+        })
         let imput = document.querySelector('input');
         itemLista.innerText = imput.value
         imput.value = '';
@@ -14,3 +17,16 @@ function criarTarefa() {
     }) // tive explicação na mentoria!!!!
 }
 criarTarefa();
+
+/* function changeColorItenList() {
+    let itens = document.querySelectorAll('li');
+    console.log(itens)
+    if (itens.length > 0) {
+            for (let i = 0; i <itens.length; i += 1) {
+            itens[i].addEventListener('click', function() {
+                itens[i].style.backgroundColor = 'gray'; 
+            })
+        }
+    }    
+}
+changeColorItenList(); */
