cb5e3ca0f855b197058f71e1be6d88226d0a4a99
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-02-16 21:05:49 -0300

    entrega requisitos 9

--- script.js
+++ script.js
@@ -4,29 +4,21 @@ function criarTarefa() {
     botao.addEventListener('click', function() {
         let lista = document.getElementById('lista-tarefas');
         let itemLista = document.createElement('li');
-        //função para ficar cinza
+        //pra deixar lista cinza
         itemLista.addEventListener('click', function() {
-                itemLista.style.backgroundColor = 'rgb(128, 128, 128)'
-            
+            itemLista.style.backgroundColor = 'rgb(128, 128, 128)';
+        })
+        //pra deixar riscado
+        itemLista.addEventListener('dblclick', function() {
+            if(itemLista.className !== 'completed') {
+                itemLista.className = 'completed';
+            } else {itemLista.className = '';
+        }
         })
         let imput = document.querySelector('input');
         itemLista.innerText = imput.value
         imput.value = '';
-
         lista.appendChild(itemLista);
     }) // tive explicação na mentoria!!!!
 }
 criarTarefa();
-
-/* function changeColorItenList() {
-    let itens = document.querySelectorAll('li');
-    console.log(itens)
-    if (itens.length > 0) {
-            for (let i = 0; i <itens.length; i += 1) {
-            itens[i].addEventListener('click', function() {
-                itens[i].style.backgroundColor = 'gray'; 
-            })
-        }
-    }    
-}
-changeColorItenList(); */
