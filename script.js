d12dfe70d9f63d1bcf638b3f3b8c68a42cd58dec
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-02-17 14:00:11 -0300

    entrega requisitos 8

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 function criarTarefa() {
     let botao = document.getElementById('criar-tarefa');
-        /* console.log(lista); */
-    botao.addEventListener('click', function() {
-        let lista = document.getElementById('lista-tarefas');
+    // para criar tarefa
+    botao.addEventListener('click', function () {
+        let lista = document.querySelector('#lista-tarefas');
         let itemLista = document.createElement('li');
-        //pra deixar lista cinza
-        itemLista.addEventListener('click', function() {
-            itemLista.style.backgroundColor = 'rgb(128, 128, 128)';
-        })
-        //pra deixar riscado
-        itemLista.addEventListener('dblclick', function() {
-            if(itemLista.className !== 'completed') {
-                itemLista.className = 'completed';
-            } else {itemLista.className = '';
-        }
-        })
         let imput = document.querySelector('input');
         itemLista.innerText = imput.value
         imput.value = '';
-        lista.appendChild(itemLista);
 
+        //pra deixar lista cinza
+        itemLista.addEventListener('click', function () {
+            let selecionado = document.querySelector('.selected');
+            if (selecionado !== null) {
+                selecionado.className = '';
+                selecionado.style.backgroundColor = 'white';
+            }
+            itemLista.className = 'selected';
+            itemLista.style.backgroundColor = 'rgb(128, 128, 128)';
+
+        })
+    
+        //pra deixar riscado
+        itemLista.addEventListener('dblclick', function () {
+            if (itemLista.className !== 'completed') {
+                itemLista.className = 'completed';
+            } else {
+                itemLista.className = '';
+            }         
+        })
+
+        lista.appendChild(itemLista);
+        
         //para apagar linhas
         let botaoApaga = document.querySelector('#apaga-tudo');
-        botaoApaga.addEventListener('click', function() {
+        botaoApaga.addEventListener('click', function () {
             itemLista.remove();
         })
 
+        //para apagar concluidas
         let botaoConcluidas = document.querySelector('#remover-finalizados');
-        botaoConcluidas.addEventListener('click', function() {
+        botaoConcluidas.addEventListener('click', function () {
             if (itemLista.className === 'completed') {
                 itemLista.remove();
             }
